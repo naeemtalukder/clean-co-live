753863534020c640a20ebb77380459f9f22934e8
naeemtalukder <naeemtalukder169135@gmail.com>
2023-01-07 23:08:57 +0600

    Banner & Input group done

--- src/page/home/Landing.js
+++ src/page/home/Landing.js
@@ -16,7 +16,7 @@ const Landing = () => {
                 </div>
             </div>
         </div>
-        <div className="rounded-2xl mx-auto mt-[-50px] relative z-20 shadow-lg p-10 bg-base-100 w-5/6">
+        <div className="rounded-2xl mx-auto mt-[-50px] relative z-20 shadow-lg p-10 bg-base-200 w-5/6">
             <h1 className="text-2xl mb-5 text-primary">Get Free Estimate</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                 <input type="text" placeholder="Type here" className="input input-bordered w-full" />
@@ -28,6 +28,7 @@ const Landing = () => {
                 <input type="text" placeholder="Type here" className="input input-bordered w-full" />
                 <input type="text" placeholder="Type here" className="input input-bordered w-full" />
             </div>
+            <button className="btn btn-primary mt-5">Get a Quote</button>
         </div>
         </div>
     );
