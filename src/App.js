e89bb4f28e7fe7e9759b674a2433129ef9f02a34
naeemtalukder <naeemtalukder169135@gmail.com>
2023-01-07 23:34:54 +0600

    Landing Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { publicRoute } from "./routes/publicRoute";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   
   return (
     <>
