3c617761d086dad428ac4f9c1c7beefabe8fb9b9
naeemtalukder <naeemtalukder169135@gmail.com>
2023-01-10 20:34:19 +0600

    PrivateRoute bug fix

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { publicRoute } from "./routes/publicRoute";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
-import Services from "./page/Services";
+// import Services from "./page/Services";
 import PrivateRoute from "./authentication/PrivateRoute";
-import About from "./page/About";
+// import About from "./page/About";
 import { privateRoute } from "./routes/privateRoute";
 
 
@@ -23,8 +23,9 @@ function App() {
             <Route key={index} path={path} element={<Component />} />
           ))}
           <Route element={<PrivateRoute />}>
-            <Route path="about" element={<About/>} />
-            <Route path="services" element={<Services/>} />
+          {privateRoute.map(({path, Component}, index) => (
+            <Route key={index} path={path} element={<Component />} />
+          ))}
           </Route>
         </Routes>
       </Navbar>
--- src/routes/privateRoute.js
+++ src/routes/privateRoute.js
@@ -4,4 +4,4 @@ import Services from "../page/Services";
 export const privateRoute = [
     {path:"/about", name: "About", Component: About},
     {path:"/services", name: "Services", Component: Services},
-  ]
+  ];
