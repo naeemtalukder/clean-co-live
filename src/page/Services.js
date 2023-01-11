6907d9f392e42bf7390f86753ffa8b3f55ef2350
naeemtalukder <naeemtalukder169135@gmail.com>
2023-01-11 23:21:59 +0600

    axios add

--- src/page/Services.js
+++ src/page/Services.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        axios
+        .get("http://localhost:5000/service")
+        .then((res) => setServices(res.services));
+    }, []);
     return (
         <div>
             <h1>This is Services Page</h1>
