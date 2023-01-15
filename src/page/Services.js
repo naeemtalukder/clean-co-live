b047619b0420fea9dfadaa8de11299d2babb7b3c
naeemtalukder <naeemtalukder169135@gmail.com>
2023-01-15 23:07:23 +0600

    'Add service' route add

--- src/page/Services.js
+++ src/page/Services.js
@@ -1,12 +1,18 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import fetcher from '../api';
 
 const Services = () => {
     const [services, setServices] = useState([]);
+    console.log(services);
+  
     useEffect(() => {
-        axios
-        .get("http://localhost:5000/service")
-        .then((res) => setServices(res.services));
+      // axios.get("http://localhost:5000/service").then((res) => setServices(res));
+  
+      (async () => {
+        const res = await fetcher.get("/service");
+        setServices(res.data);
+      })();
     }, []);
     return (
         <div>
--- src/page/dashboard/AddServices.js
+++ src/page/dashboard/AddServices.js
@@ -2,14 +2,32 @@ import React from 'react';
 
 const AddServices = () => {
     return (
-        <div>
-            <h1>This is Add Services</h1>
-            <h1>This is Add Services</h1>
-            <h1>This is Add Services</h1>
-            <h1>This is Add Services</h1>
-            <h1>This is Add Services</h1>
-            <h1>This is Add Services</h1>
+        <div className="hero min-h-screen bg-accent mt-8">
+        <div className="hero-content">
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <h1 className='text-2xl text-center'>Add Services</h1>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Service Name</span>
+                        </label>
+                        <input type="text" placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                        <span className="label-text">Service Charge</span>
+                        </label>
+                        <input type="text" placeholder="password" className="input input-bordered" />
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+                    <div className="divider">OR</div>
+                    <button className='btn btn-glass hover:btn-accent'>Login with Google</button>
+                </div>
+            </div>
         </div>
+    </div>
     );
 };
 
